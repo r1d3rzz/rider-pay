b58670650947eedb91d93938d844f24166cd6f34
r1d3rzz <myintthwaykhine127@gmail.com>
2024-02-11 21:44:20 +0630

    lara/breeze install and setup auth route customize

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Auth\AdminLoginController;
+use App\Http\Controllers\Frontend\PageController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//For Admin Users
+Route::prefix('admin')->group(function () {
+    Route::controller(AdminLoginController::class)->group(function () {
+        Route::get('login', 'create')->name('admin.login');
+        Route::post('login', 'store')->name('admin.login');
+        Route::post('logout', 'destroy')->name('admin.logout');
+    });
+
+    Route::get('/', function () {
+        return 'admin';
+    })->name('admin.home');
+});
+
+//For Users
+Route::middleware('auth')->group(function () {
+    Route::get("/", [PageController::class, 'home']);
+});
+
+
+require __DIR__ . '/auth.php';
